ef0314ddc574a956740277c60113b74dc08143fa
Roelof Reitsma <roelof.reitsma@jumbo.com>
2020-06-11 20:37:26 +0200

    Make scope read:me required instead of read:jira-user
    It simplifies the code a lot and is clearer

--- lib/omniauth/strategies/atlassian_oauth2.rb
+++ lib/omniauth/strategies/atlassian_oauth2.rb
@@ -24,16 +24,19 @@ module OmniAuth
              audience: 'api.atlassian.com'
 
       uid do
-        raw_info['myself']['accountId']
+        raw_info['myself']['account_id']
       end
 
       info do
         {
-          name: raw_info['myself']['displayName'],
-          email: raw_info['myself']['emailAddress'],
-          nickname: raw_info['myself']['name'],
-          location: raw_info['myself']['timeZone'],
-          image: raw_info['myself']['avatarUrls']['48x48']
+          name: raw_info['myself']['name'],
+          email: raw_info['myself']['email'],
+          nickname: raw_info['myself']['nickname'],
+          location: raw_info['myself']['extended_profile']['location'],
+          job_title: raw_info['myself']['extended_profile']['job_title'],
+          image: raw_info['myself']['picture'],
+          account_status: raw_info['myself']['account_status'],
+          timezone: raw_info['myself']['zoneinfo']
         }
       end
 
@@ -46,29 +49,10 @@ module OmniAuth
       def raw_info
         return @raw_info if @raw_info
 
-        # NOTE: api.atlassian.com, not auth.atlassian.com!
-        accessible_resources_url = 'https://api.atlassian.com/oauth/token/accessible-resources'
-        sites = JSON.parse(access_token.get(accessible_resources_url).body)
-
-        # Jira's OAuth gives us many potential sites. To request information
-        # about the user for the OmniAuth hash, pick the first one that has the
-        # necessary 'read:jira-user' scope.
-        jira_user_scope = 'read:jira-user'
-        site = sites.find do |candidate_site|
-          candidate_site['scopes'].include?(jira_user_scope)
-        end
-        unless site
-          raise "No site found with scope #{jira_user_scope}, please ensure the scope ${jira_user_scope} is added to your OmniAuth config"
-        end
-
-        cloud_id = site['id']
-        base_url = "https://api.atlassian.com/ex/jira/#{cloud_id}"
-        myself_url = "#{base_url}/rest/api/3/myself"
-
-        myself = JSON.parse(access_token.get(myself_url).body)
+        sites = JSON.parse(access_token.get('https://api.atlassian.com/oauth/token/accessible-resources').body)
+        myself = JSON.parse(access_token.get('https://api.atlassian.com/me').body)
 
         @raw_info ||= {
-          'site' => site,
           'sites' => sites,
           'myself' => myself
         }
